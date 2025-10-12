5d51475de8fac57ec65c16d6ceca98549412380a
Royston <roystonad2004@gmail.com>
2025-10-12 18:37:42 +0530

    feat: Initialize portfolio project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development and build.
    - Created main application structure in App.jsx.
    - Implemented custom cursor component.
    - Added navbar with overlay menu for navigation.
    - Developed particles background effect for visual enhancement.
    - Created sections for About, Contact, Experience, Footer, Home, Projects, Skills, and Testimonials.
    - Included necessary assets such as images and logos.
    - Configured Vite for React and Tailwind CSS integration.
    - Added global styles in index.css.

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import CustomCursor from "./components/CustomCursor";
+import Navbar from "./components/Navbar";
+import ParticlesBackground from "./components/ParticlesBackground";
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Experience from "./sections/Experience";
+import Footer from "./sections/Footer";
+import Home from "./sections/Home";
+import Projects from "./sections/Projects";
+import Skills from "./sections/Skills";
+import Testimonials from "./sections/Testimonials";
+
+export default function App() {
+  return (
+    <div className="relative gradient text-white scroll-smooth">  
+    <CustomCursor/>
+    <ParticlesBackground/>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Experience/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </div>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import OverlayMenu from './OverlayMenu'
+import Logo from '../assets/logo.png'
+import { FiMenu } from 'react-icons/fi';
+const Navbar = () => {
+
+    const [menuOpen,setMenuOpen]=useState(false);
+    const [visible,setVisible]=useState(true);
+
+  return (
+    <>
+    <nav className={`fixed top-0 left-0 flex w-full px-6 py-4 items-center z-50 transition-transform duration-200 ${visible?"translate-y-0":"-translate-y-full"} justify-between`}>
+<div className='flex items-center space-x-2'>
+    <img src={Logo} className="w-8 h-8"/>
+    <div className='text-2xl font-bold text-white hidden sm:block'>Royston Akash Dsouza</div>
+</div>
+<div className='block lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2'>
+<button className='text-white text-3xl focus:outline-none' aria-label="openmenu" onClick={()=>setMenuOpen(true)} >
+    <FiMenu/>
+</button>
+</div>
+<div className='hidden lg:block '>
+<a href='#contact' className='bg-gradient-to-r from-pink-500 to-blue-500 text-white px-5 py-2 rounded-full font-medium shadow-lg hover:opacity-90 transition-opacity duration-300'>
+    Reach out
+</a>
+</div>
+    </nav>
+    <OverlayMenu isOpen={menuOpen} onClose={()=>setMenuOpen(false)}/>
+    </>
+  )
+}
+
+export default Navbar
