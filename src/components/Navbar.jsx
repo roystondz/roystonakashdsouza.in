24895bbc35672838b15881b5d0866b9eb80a8755
Royston <roystonad2004@gmail.com>
2025-10-12 19:10:46 +0530

    feat: Enhance Navbar component with scroll visibility and update logo styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import OverlayMenu from './OverlayMenu'
 import Logo from '../assets/Logo.png'
 import { FiMenu } from 'react-icons/fi';
@@ -6,13 +6,62 @@ const Navbar = () => {
 
     const [menuOpen,setMenuOpen]=useState(false);
     const [visible,setVisible]=useState(true);
+    const [forceVisible,setForceVisible]=useState(false);
+
+
+    const lastScrollY=useRef(0);
+    const timerId=useRef(null);
+
+    useEffect(()=>{
+        const homeSection=document.querySelector('#home');
+        const observer=new IntersectionObserver(([entry])=>{
+            if(entry.isIntersecting){
+                setForceVisible(true);
+                setVisible(true);
+            }else{
+                setForceVisible(false);
+
+            }
+        }   ,{threshold:0.1})
+        if (homeSection) observer.observe(homeSection);
+
+        return(()=>{
+            if (homeSection) observer.unobserve(homeSection);
+        })
+    },[])
+
+    useEffect(()=>{
+        const handleScroll=()=>{
+            if (forceVisible) {
+                setVisible(true);
+            return;
+            }
+        const currentScrollY=window.scrollY;
+        if (currentScrollY>lastScrollY.current && currentScrollY>100){
+            setVisible(false);
+        }else{
+            setVisible(true);
+            if (timerId.current) clearTimeout(timerId.current);
+            timerId.current=setTimeout(()=>{
+                setVisible(false);
+            },3000);
+        }
+        lastScrollY.current=currentScrollY;
+        
+}
+        window.addEventListener('scroll',handleScroll);
+        return()=>{
+            window.removeEventListener('scroll',handleScroll,{passive:true});
+            if (timerId.current) clearTimeout(timerId.current);
+        }
+},[forceVisible])
 
   return (
     <>
     <nav className={`fixed top-0 left-0 flex w-full px-6 py-4 items-center z-50 transition-transform duration-200 ${visible?"translate-y-0":"-translate-y-full"} justify-between`}>
 <div className='flex items-center space-x-2'>
-    <img src={Logo} className="w-8 h-8"/>
-    <div className='text-2xl font-bold text-white hidden sm:block'>Royston Akash Dsouza</div>
+    <img src={Logo} className="w-8 h-8 "/>
+    <div className='text-2xl font-bold text-white hidden sm:block hover:text-pink-500 transition-colors duration-300'>Royston Akash Dsouza</div>
 </div>
 <div className='block lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2'>
 <button className='text-white text-3xl focus:outline-none' aria-label="openmenu" onClick={()=>setMenuOpen(true)} >
